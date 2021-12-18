2d13c01cb079033c8c0cb8438d9eae2f941eb9c1
muliann <muliann582@gmail.com>
2021-12-18 11:42:00 +0300

    iot code

--- iot.ino
+++ iot.ino
@@ -4,12 +4,14 @@
 #include <WiFiClient.h>
 
 /* Set these to your desired credentials. */
-const char* ssid = "Zion";  //ENTER YOUR WIFI SETTINGS
-const char* password = "Kabu2020";
-const char* serverName = "192.168.1.107";
+const char* ssid = "Muli";  //ENTER YOUR WIFI SETTINGS
+const char* password = "12345678";
+const char* serverName = "192.168.43.140";
 
 // am using 3 sensors for if one works everything else will.
-int slot1 = 2;   // GPIO2 = D4
+int slot1 = 16;   // GPIO2 = D0
+int slot2 = 4;   // GPIO4 = D2
+int slot3 = 5;   // GPIO5 = D1
 
 void setup() {
   delay(1000);
@@ -46,42 +48,43 @@ void loop() {
   WiFiClient client;
 
    int sensor1 = digitalRead(slot1);
-   String string = "sensor=";
+     int sensor2 = digitalRead(slot2);
+   int sensor3 = digitalRead(slot3);
+   String string1 = "sensor1=";
+   String string2 = "sensor2=";
+   String string3 = "sensor3=";
+   String status1 = string1+sensor1;
+   String status2 = string2+sensor2; 
+   String status3 = string3+sensor3;
   
   if (client.connect(serverName, 80)) {
     Serial.println("connected to server");
-
+    String httpRequestData =String(sensor1) +String(sensor2)+String(sensor3);
     String postStr;
-    postStr += "http://192.168.1.107:80/smart_parking/test.php?sensor1=";
-    postStr += String(sensor1);
+    postStr += "http://192.168.43.140:80/smart_parking/sensordata.php?httpRequestData=";
+    postStr += String(httpRequestData);
 
     http.begin(postStr);      //Specify request destination
     http.addHeader("Content-Type", "text/plain");  //Specify content-type header
     
-   String httpRequestData = string + sensor1 ;
-   Serial.print("httpRequestData: ");
-   Serial.println(httpRequestData);
+  
+    Serial.print("httpRequestData: ");
+    Serial.println(httpRequestData);
    
-   int httpResponseCode = http.POST(httpRequestData);   //Send the request
-   if (httpResponseCode > 0) {
+    int httpResponseCode = http.POST(httpRequestData);   //Send the request
+    if (httpResponseCode > 0) {
       Serial.print("HTTP Response code: ");
       Serial.println(httpResponseCode);
       String response = http.getString();
       Serial.println(response);
-    }
-    else {
+      }
+     else {
       Serial.print("Error code: ");
       Serial.println(httpResponseCode);
-    }
+     }
     http.end();  //Close connection
   }
+  
   delay(2000);
   }
 }
-
-
-
-
-
-
-   
